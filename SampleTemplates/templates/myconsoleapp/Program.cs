f0483edd75521875f123c1c866d562ff9584f454
Billy Wolfington <willwolfram18@gmail.com>
2019-06-21 22:24:28 -0700

    Initial commit

---
+++ SampleTemplates/templates/myconsoleapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example custom template.");
+        }
+    }
+}
