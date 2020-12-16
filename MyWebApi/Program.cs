23a3d250c36ebbee7906c0fb82f418dc791ec9a4
William Wolfram <willwolfram18@gmail.com>
2020-12-15 22:43:16 -0800

    Program file set up

--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,11 +14,33 @@ namespace MyWebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            var log = host.Services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                log.LogInformation("Starting application...");
+
+                host.Run();
+            } catch (Exception e)
+            {
+                log.LogCritical(e, "Application encountered an error.");
+            }
+            finally
+            {
+                log.LogInformation("Application shutting down");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddEventSourceLogger();
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddLog4Net("log4net.config");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
